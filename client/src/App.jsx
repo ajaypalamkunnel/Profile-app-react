9f32c274c6df3d7d2382d8b639bc2ea4c98460ba
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-01-09 15:31:41 +0530

    Router and pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Sigin from "./pages/sigin";
+import Signup from "./pages/SignUp";
+import Profile from "./pages/profile";
 
 const App = () => {
   return (
-    <div className='text-red-800'>App</div>
-  )
-}
+    <BrowserRouter>
+     <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<Sigin/>}/>
+        <Route path="/Sigun-up" element={<Signup/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+     </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
