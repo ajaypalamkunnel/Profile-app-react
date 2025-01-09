69baa5bdc4309ec83f2647d0231e808026f080c2
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-01-09 19:45:36 +0530

    mongo db connect but atlass connection is not working so compass is connected now, planning to connect to atlass when deploy time

--- api/index.js
+++ api/index.js
@@ -1,8 +1,25 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+
+  console.log(process.env.MONGODB_URI) // Correct connection string
+ 
+    
+  mongoose
+  .connect(process.env.MONGODB_URI) // Correct connection string
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  });
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!')
-})
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
