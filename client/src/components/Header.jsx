c3236ade645e7ec7a1771c4c1b9d22ee0d49e2fd
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-01-10 09:41:41 +0530

    complete sigup Page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Links } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to='/home'>
-        <h1 className="font-bold">Profile App</h1>
+        <Link to="/home">
+          <h1 className="font-bold">Profile App</h1>
         </Link>
         <ul className="flex gap-4">
           <Link to="/">
@@ -15,8 +15,12 @@ const Header = () => {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <li to="/sign-in">Sign In</li>
-          <li to="/profile">Profile</li>
+          <Link to="/sign-in">
+            <li>Sign In</li>
+          </Link>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
         </ul>
       </div>
     </div>
--- client/src/pages/Sigin.jsx
+++ client/src/pages/Sigin.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Sigin = () => {
   return (
-    <div>S</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
 
-export default Sigin
+      </form>
+      <div className="flex gap-2 mt-5">
+      <p>Have an accout </p>
+      <Link to='/signin'><span className="text-blue-600 ">sign In</span></Link>
+
+      </div>
+    </div>
+  );
+};
+
+export default Sigin;
