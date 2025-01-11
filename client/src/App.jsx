cef1ac01d423f667835389e888b5bda0b7a7e7b3
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-01-11 13:04:48 +0530

    route protection for header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Sigin from "./pages/Signin";
 import Signup from "./pages/Sigup";
 import Profile from "./pages/profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -16,7 +17,9 @@ const App = () => {
         <Route path="/about" element={<About/>}/>
         <Route path="/sign-in" element={<Sigin/>}/>
         <Route path="/Sign-up" element={<Signup/>}/>
+        <Route element={PrivateRoute}>
         <Route path="/profile" element={<Profile/>}/>
+        </Route>
      </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, Links } from "react-router-dom";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,12 +17,22 @@ const Header = () => {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/sign-in">
-            <li>Sign In</li>
-          </Link>
-          <Link to="/profile">
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                className="h-7 w-7 rounded-full object-cover"
+                src={currentUser.profilePicture}
+                alt="Profile"
+              />
+            </Link>
+          ) : (
+            <Link to="/sign-in">
+              <li>Sign In</li>
+            </Link>
+          )}
+          {/* <Link to="/profile">
             <li>Profile</li>
-          </Link>
+          </Link> */}
         </ul>
       </div>
     </div>
