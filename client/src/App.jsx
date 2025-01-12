16b089daddb288fac39c62b62b4e0087d0ee4e59
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2025-01-12 15:00:03 +0530

    Route protection completely done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 import Sigin from "./pages/Signin";
 import Signup from "./pages/Sigup";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 const App = () => {
   return (
@@ -15,8 +16,10 @@ const App = () => {
      <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route element={<PublicRoute/>}>
         <Route path="/sign-in" element={<Sigin/>}/>
         <Route path="/Sign-up" element={<Signup/>}/>
+        </Route>
         <Route element={<PrivateRoute/>}>
         <Route path="/profile" element={<Profile/>}/>
         </Route>
